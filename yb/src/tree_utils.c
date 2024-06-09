2480c73dfda53fc33dfdfd22f04ab0188ba7ef04
Yubeen Lee <lybin71@naver.com>
2024-06-09 19:06:54 +0100

    rediretion added, edge case not handled yet

---
+++ yb/src/tree_utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tree_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 18:56:33 by yublee            #+#    #+#             */
+/*   Updated: 2024/06/09 19:03:16 by yublee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+void	print_node(void *item)
+{
+	ft_printf("%s\n", item);
+}
+
+void	free_node(t_btree *root)
+{
+	free(root->item);
+	free(root);
+}
+
+t_btree	*create_node(void *item)
+{
+	t_btree	*node;
+
+	node = (t_btree *)malloc(sizeof(t_btree));
+	if (!node)
+		exit (1);
+	node->left = NULL;
+	node->right = NULL;
+	node->item = item;
+	return (node);
+}
+
+void	btree_apply_infix(t_btree *root, void (*applyf)(void *))
+{
+	if (root == NULL)
+		return ;
+	btree_apply_infix(root->left, applyf);
+	applyf(root->item);
+	btree_apply_infix(root->right, applyf);
+}
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(t_btree *))
+{
+	if (root == NULL)
+		return ;
+	btree_apply_suffix(root->left, applyf);
+	btree_apply_suffix(root->right, applyf);
+	applyf(root);
+}
