6074615dc7dcb7780289a028ca680d47c5878e00
Yubeen Lee <lybin71@naver.com>
2024-06-10 22:33:00 +0100

    works well with multiple pipes, norminette/memory:OK, now needs to handle heredoc

--- yb/src/pipex.c
+++ yb/src/pipex.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex_init.c                                       :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 00:37:53 by yublee            #+#    #+#             */
-/*   Updated: 2024/06/10 19:41:56 by yublee           ###   ########.fr       */
+/*   Updated: 2024/06/10 22:28:10 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	free_fds(int **fds, int i)
-{
-	int	j;
-
-	j = 0;
-	while (j < i)
-		free(fds[j++]);
-	free(fds);
-	return (1);
-}
 
 static t_info	get_info(t_list **cmd_list, t_btree *root, char **env)
 {
@@ -61,11 +50,61 @@ static int	**create_pipeline(int cnt)
 	return (fds);
 }
 
+static void	child_process(int i, t_list *current, t_info info)
+{
+	t_btree	*cmd;
+	char	*cmd_str;
+	char	**args;
+
+	cmd = (t_btree *)current->content;
+	get_input(cmd, i, info);
+	get_output(cmd, i, info);
+	cmd_str = cmd->item;
+	if (cmd_str[0] == 0)
+		exit (EXIT_SUCCESS);
+	args = get_args(cmd_str, info.env, info);
+	if (execve(args[0], args, info.env) == -1)
+	{
+		free_str_array(args);
+		exit_with_error("execve", EXIT_FAILURE, info);
+	}
+}
+
+void	exec_pipex(t_info info, t_list **cmd_list, int *status)
+{
+	int		i;
+	pid_t	pid;
+	t_list	*current;
+
+	i = -1;
+	current = *cmd_list;
+	while (info.cmd_cnt && ++i < info.cmd_cnt)
+	{
+		pid = fork();
+		if (pid < 0)
+			exit_with_error("fork", EXIT_FAILURE, info);
+		if (pid == 0)
+			child_process(i, current, info);
+		if (i != 0)
+			close(info.fds[i - 1][READ_END]);
+		if (i != info.cmd_cnt - 1)
+			close(info.fds[i][WRITE_END]);
+		current = current->next;
+	}
+	waitpid(pid, status, 0);
+	free_before_exit(info);
+	while (wait(NULL) != -1)
+		;
+}
+
 void	pipex(t_list **cmd_list, t_btree *root, char **env)
 {
 	t_info	info;
+	int		status;
 
+	status = 0;
 	info = get_info(cmd_list, root, env);
 	info.fds = create_pipeline(info.cmd_cnt - 1);
-	exec_pipex(info, cmd_list);
+	exec_pipex(info, cmd_list, &status);
+	exit(WEXITSTATUS(status));
 }
--- yb/src/utils.c
+++ yb/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 14:59:19 by yublee            #+#    #+#             */
-/*   Updated: 2024/06/10 17:45:48 by yublee           ###   ########.fr       */
+/*   Updated: 2024/06/10 22:12:52 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,47 @@ void	ft_strtrim_and_free(t_btree *root)
 	result = ft_strtrim(root->item, " ");
 	free(root->item);
 	root->item = result;
+}
+
+void	free_before_exit(t_info	info)
+{
+	if (info.fds)
+		free_fds(info.fds, info.cmd_cnt - 1);
+	ft_lstclear(info.cmd_list, del);
+	btree_apply_suffix(info.root, free_node);
+}
+
+void	exit_with_error(char *str, int exit_no, t_info info)
+{
+	if (exit_no == 127)
+	{
+		write(2, str, ft_strlen(str));
+		write(2, ": command not found\n", 20);
+		free(str);
+	}
+	else
+		perror(str);
+	free_before_exit(info);
+	exit(exit_no);
+}
+
+void	free_str_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free(array);
+}
+
+int	free_fds(int **fds, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i)
+		free(fds[j++]);
+	free(fds);
+	return (1);
 }
